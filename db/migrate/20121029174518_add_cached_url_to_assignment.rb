f7f3cf0d872515c94fe9ff8c067d45f4f06a631e
ogg <neal.lawson@gmail.com>
2013-03-01 18:07:53 -0800

    db mods

---
+++ db/migrate/20121029174518_add_cached_url_to_assignment.rb
@@ -0,0 +1,10 @@
+class AddCachedUrlToAssignment < ActiveRecord::Migration
+  tag :predeploy
+  def self.up
+  	 add_column :assignments, :cached_url, :string
+  end
+
+  def self.down
+  	remove_column :assignments, :cached_url
+  end
+end
