3b158e64464a22faec3b27745179791efde3c708
ogg <neal.lawson@gmail.com>
2013-03-11 16:16:04 -0700

    adding support for multitennitcy

--- app/middleware/load_account.rb
+++ app/middleware/load_account.rb
@@ -5,17 +5,28 @@ class LoadAccount
 
   def call(env)
     Account.clear_special_account_cache!
-    domain_root_account = LoadAccount.default_domain_root_account
+    #domain_root_account = LoadAccount.default_domain_root_account
+    domain_root_account = empowered_root_account
+    Rails.logger.info "Empowered: LOADING ACCOUNT: #{domain_root_account.id}:#{domain_root_account.name}"
     configure_for_root_account(domain_root_account)
 
     env['canvas.domain_root_account'] = domain_root_account
     @app.call(env)
+
   end
 
   def self.default_domain_root_account; Account.default; end
 
-  protected
+  def empowered_root_account
+    if Empowered_config[:root_account]
+      Account.find Empowered_config[:root_account]
+    else
+      Rails.logger.info "Empowered: NO ACCOUNT FOUND LOADING CANVAS DEFAULT"
+      LoadAccount.default_domain_root_account
+    end
+  end
 
+  protected
   def configure_for_root_account(domain_root_account)
     Attachment.domain_namespace = domain_root_account.file_namespace
   end
