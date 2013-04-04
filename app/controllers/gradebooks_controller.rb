3e1e1d3735def62e70b60a18fef483de2b08a94e
ogg <nlawson@empowered.com>
2013-04-04 10:54:06 -0700

    lets force gradebook2 as default

--- app/controllers/gradebooks_controller.rb
+++ app/controllers/gradebooks_controller.rb
@@ -407,7 +407,14 @@ class GradebooksController < ApplicationController
   end
 
   def redirect_to_appropriate_gradebook_version
-    redirect_to gradebook_url_for(@current_user, @context)
+    #redirect_to gradebook_url_for(@current_user, @context)
+    if !Empowered_config[:old_grade_book_enabled]
+      @current_user.preferences[:use_gradebook2] = true
+      @current_user.save!
+      redirect_to named_context_url(@context, "context_gradebook2_url")
+    else
+      redirect_to gradebook_url_for(@current_user, @context)
+    end
   end
   protected :redirect_to_appropriate_gradebook_version
 
