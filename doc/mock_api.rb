fd9389331cc22ee5d2f476d25616f1c2167b92df
ogg <nlawson@empowered.com>
2013-04-09 14:59:06 -0700

    adding mock-api

---
+++ doc/mock_api.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+# sudo ./mock_api.rb -p 80
+
+require 'rubygems'
+require 'sinatra'
+require 'pp'
+require 'json'
+
+post '/api/messages/fromcanvas' do
+  data = JSON.parse(request.body.read.to_s)
+  logger.info data
+  pp data
+end
