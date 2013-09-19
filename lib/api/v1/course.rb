e1cffdf43ba55f6fb18bf2e669084e87db5d27ad
Dan Warter <danielappinfo@gmail.com>
2013-09-18 17:29:52 -0700

    updating course api

--- lib/api/v1/course.rb
+++ lib/api/v1/course.rb
@@ -29,12 +29,68 @@ module Api::V1::Course
   end
 
   def course_json(course, user, session, includes, enrollments)
-    Api::V1::CourseJson.to_hash(course, user, includes, enrollments) do |builder, allowed_attributes, methods|
-      hash = api_json(course, user, session, :only => allowed_attributes, :methods => methods)
-      hash['term'] = enrollment_term_json(course.enrollment_term, user, session, {}) if includes.include?('term')
-      hash['apply_assignment_group_weights'] = course.apply_group_weights?
-      add_helper_dependant_entries(hash, course, builder)
+  #Empowered: taking out CourseJson module and customizing API
+    #Api::V1::CourseJson.to_hash(course, user, includes, enrollments) do |builder, allowed_attributes, methods|
+    #  hash = api_json(course, user, session, :only => allowed_attributes, :methods => methods)
+    #  hash['term'] = enrollment_term_json(course.enrollment_term, user, session, {}) if includes.include?('term')
+    #  hash['apply_assignment_group_weights'] = course.apply_group_weights?
+    #  add_helper_dependant_entries(hash, course, builder)
+    #end
+    include_grading = includes.include?('needs_grading_count')
+    include_syllabus = includes.include?('syllabus_body')
+    include_total_scores = includes.include?('total_scores') && !course.settings[:hide_final_grade]
+    include_url = includes.include?('html_url')
+    include_description = includes.include?('public_description')
+
+    base_attributes = %w(id name course_code account_id start_at)
+    allowed_attributes = includes.is_a?(Array) ? base_attributes + includes : base_attributes
+    hash = api_json(course, user, session, :only => allowed_attributes, :methods => 'end_at')
+    case course.workflow_state
+      when "claimed"
+        hash['state'] = "unpublished"
+      when "available"
+        hash['state'] = "published"
     end
+    hash['sis_course_id'] = course.sis_source_id if course.root_account.grants_rights?(user, :read_sis, :manage_sis).values.any?
+    ["start_at","conclude_at"].each {|date|
+      hash[date] = course.send(date.to_sym)
+    }
+    has_muted_assignments = false
+    course.assignments.each do |a|
+      if a.muted? == true
+        has_muted_assignments = true
+      end
+    end
+    if enrollments
+      hash['enrollments'] = enrollments.map do |e|
+        h = { :type => e.readable_type.downcase }
+        if include_total_scores && e.student?
+          h.merge!(
+              :computed_current_score => e.computed_current_score,
+              :computed_final_score => e.computed_final_score,
+              :computed_final_grade => e.computed_final_grade,
+              :computed_current_grade => e.computed_current_grade,
+              :has_muted_assignments => has_muted_assignments)
+        end
+        h
+      end
+    end
+    hash['calendar'] = { 'ics' => "#{feeds_calendar_url(course.feed_code)}.ics" }
+    hash['announcement_feed'] = "http://#{HostUrl.context_host(course)}/feeds/announcements/course_#{course.uuid}.atom"
+    if include_grading && enrollments && enrollments.any? { |e| e.participating_instructor? }
+      hash['needs_grading_count'] = course.assignments.active.sum('needs_grading_count')
+    end
+
+    if include_syllabus
+      hash['syllabus_body'] = course.syllabus_body
+    end
+
+    if include_description
+      hash['public_description'] = course.public_description
+    end
+    request = self.respond_to?(:request) ? self.request : nil
+    hash['html_url'] = course_url(course, :host => HostUrl.context_host(course, request.try(:host_with_port))) if include_url
+    hash
   end
 
   def copy_status_json(import, course, user, session)
@@ -50,6 +106,7 @@ module Api::V1::Course
 
     hash[:status_url] = api_v1_course_copy_status_url(course, import)
     hash
+  #Empowered end
   end
 
   def add_helper_dependant_entries(hash, course, builder)
