c90a57364c85568f5600d3830344d3a96f9fcd12
ogg <nlawson@empowered.com>
2013-10-31 11:25:52 -0700

    oops

--- config/shared_boot.rb
+++ config/shared_boot.rb
@@ -54,7 +54,7 @@ end
 #config.active_record.observers = [:cacher, :stream_item_cache]
 
 if File.exist? ("#{RAILS_ROOT}/config/observer_config.json")
-  include_conversation_observer = JSON.parse(File.open("#{RAILS_ROOT}/config/observer_config.json"))
+  include_conversation_observer = JSON.parse(File.read("#{RAILS_ROOT}/config/observer_config.json"))
   sym_include_conversation_observer = []
   include_conversation_observer.each {|s| sym_include_conversation_observer.push s.to_sym }
   config.active_record.observers = sym_include_conversation_observer
