4d79182353b50c2297a7743975d87f94d3c3c3a5
nlawson <ogg@Ogg-book-3.local>
2016-10-06 10:36:10 -0700

    temp fix for gradebook issue

--- app/controllers/gradebooks_controller.rb
+++ app/controllers/gradebooks_controller.rb
@@ -379,13 +379,15 @@ class GradebooksController < ApplicationController
   def redirect_to_appropriate_gradebook_version
   #Empowered: adjusting redirect for gradebook switch
     #redirect_to gradebook_url_for(@current_user, @context)
-    if !Empowered_config[:old_grade_book_enabled]
+    # if !Empowered_config[:old_grade_book_enabled]
+    #   @current_user.preferences[:use_gradebook2] = true
+    #   @current_user.save!
+    #   redirect_to named_context_url(@context, "context_gradebook2_url")
+    # else
       @current_user.preferences[:use_gradebook2] = true
       @current_user.save!
-      redirect_to named_context_url(@context, "context_gradebook2_url")
-    else
       redirect_to gradebook_url_for(@current_user, @context)
-    end
+    # end
   #Empowered end
   end
   protected :redirect_to_appropriate_gradebook_version
